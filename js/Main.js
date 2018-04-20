8e7ba5719dd283f9c426a9c036a31aab1aec11b9
Terrence <t.w.mcdonnell10@gmail.com>
2018-04-20 13:59:30 -0700

    Basic code implementation

---
+++ js/Main.js
@@ -0,0 +1,10 @@
+var canvas, canvasContext;
+
+window.onload = function() {
+	canvas = document.getElementById('gameCanvas');
+	canvasContext = canvas.getContext('2d');
+
+	colorRect(0,0, canvas.width,canvas.height, 'black');
+	colorText("LOADING IMAGES", canvas.width/2, canvas.height/2, 'white');
+	//loadImages();
+}
