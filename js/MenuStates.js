8831396ab35b94047e44be2be11d44f25ae5329a
Herleen Dualan <33649578+herleendualan@users.noreply.github.com>
2018-04-21 09:11:34 -0700

    cleaned up help screen text a bit

--- js/MenuStates.js
+++ js/MenuStates.js
@@ -13,16 +13,17 @@ function drawHelpScreen() {
 }
 
 function showHelpScreenText() {
-    colorText("How To Play", canvas.width/2, 100, "white", "30px Arial", "center", 1);
-    colorText("Movement: W, A, D", 150, 150, "white", "16px Arial", "left", 1);
-    colorText("Switch to Kitchen: SPACE, then in Kitchen Mode stop and wait for Q or E to be pressed", 150, 200, "white", "16px Arial", "left", 1);
-    colorText("Switch to Kitchen and Launch Meat: Q", 150, 250, "white", "16px Arial", "left", 1);
-    colorText("Switch to Kitchen and Launch Plant: E", 150, 300, "white", "16px Arial", "left", 1);
-    colorText("Gather Ingredient: SPACE", 150, 350, "white", "16px Arial", "left", 1);
-    colorText("Kitchen Mode:", 150, 400, "white", "16px Arial", "left", 1);
-    colorText("Movement: A, D", 150, 450, "white", "16px Arial", "left", 1);
-    colorText("Switch to Vehicle: SPACE", 150, 500, "white", "16px Arial", "left", 1);
-    colorText("Press  [SPACE]  To Return To Menu Or  [P]  to Play", canvas.width/2, 550, "white", "16px Arial", "center", 1);
+    colorText("How To Play", canvas.width/2, 50, "white", "30px Arial", "center", 1);
+    colorText("Vehicle Mode:", 150, 150, "white", "20px Arial", "left", 1);
+    colorText("Movement: W, A, D", 150, 180, "white", "16px Arial", "left", 1);
+    colorText("Switch to Kitchen: SPACE, then in Kitchen Mode stop and wait for Q or E to be pressed", 150, 210, "white", "16px Arial", "left", 1);
+    colorText("Switch to Kitchen and Launch Meat: Q", 150, 240, "white", "16px Arial", "left", 1);
+    colorText("Switch to Kitchen and Launch Plant: E", 150, 270, "white", "16px Arial", "left", 1);
+    colorText("Gather Ingredient: SPACE", 150, 300, "white", "16px Arial", "left", 1);
+    colorText("Kitchen Mode:", 150, 350, "white", "20px Arial", "left", 1);
+    colorText("Movement: A, D", 150, 380, "white", "16px Arial", "left", 1);
+    colorText("Switch to Vehicle: SPACE", 150, 410, "white", "16px Arial", "left", 1);
+    colorText("Press  [SPACE]  To Return To Menu Or  [P]  to Play", canvas.width/2, 500, "white", "16px Arial", "center", 1);
 }
 
 function setGameStates() {
